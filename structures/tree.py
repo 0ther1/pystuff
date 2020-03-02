3a4bc0587ad56c74932e719d593dc7436cd900b4
other1 <trueromanc@yandex.ru>
2020-03-03 00:55:07 +0900

    added visit, read and write functions

--- structures/tree.py
+++ structures/tree.py
@@ -1,3 +1,69 @@
+from struct import pack, unpack
+
+def nlr(node):
+    """Iteratively visit tree in pre-order starting from `node`."""
+    if (not node):
+        return
+    hist = [node]
+    while (hist):
+        node = hist.pop()
+        yield node
+        if (node.right):
+            hist.append(node.right)
+        if (node.left):
+            hist.append(node.left)
+            
+def lnr(node):
+    """Iteratively visit tree in in-order starting from `node`."""
+    hist = []
+    while (hist or node):
+        if (node):
+            hist.append(node)
+            node = node.left
+        else:
+            node = hist.pop()
+            yield node
+            node = node.right
+            
+def rnl(node):
+    """Iteratively visit tree in reverse in-order starting from `node`."""
+    hist = []
+    while (hist or node):
+        if (node):
+            hist.append(node)
+            node = node.right
+        else:
+            node = hist.pop()
+            yield node
+            node = node.left
+
+def lrn(node):
+    """Iteratively visit tree in post-order starting from `node`."""
+    hist = []
+    last_node = None
+    while (hist or node):
+        if (node):
+            hist.append(node)
+            node = node.left
+        else:
+            peek_node = hist[-1]
+            if (peek_node.right and last_node != peek_node.right):
+                node = peek_node.right
+            else:
+                yield peek_node
+                last_node = hist.pop()
+                
+def in_depth(node):
+    """Iteratively visit tree in level-order starting from `node`."""
+    queue = [node]
+    while queue:
+        node = queue.pop(0)
+        yield node
+        if (node.left):
+            queue.append(node.left)
+        if (node.right):
+            queue.append(node.right)        
+
 class BinNode():
     """Binary tree node."""
     def __init__(self, data, left=None, right=None, parent=None):
@@ -45,19 +111,6 @@ class BinTree():
             raise ValueError("Only node can be root of tree!")
         self._root = root
         self._size = 0
-        
-    def __iter__(self):
-        iterlist = []
-        current = self._get_last_left(self.root, iterlist)
-        yield current
-        while (True):
-            if (current.right):
-                current = self._get_last_left(current.right, iterlist)
-            elif (len(iterlist) > 0):
-                current = iterlist.pop()
-            else:
-                break
-            yield current
             
     def __contains__(self, value):
         for n in self:
@@ -67,17 +120,6 @@ class BinTree():
     
     def __len__(self):
         return self._size
-    
-    def _get_last_left(self, node, lst):
-        """|INTERNAL| get last left node for this node, remembering path to it.
-        Used in iterator."""
-        if (not node):
-            return None
-        current = node
-        while (current.left):
-            lst.append(current)
-            current = current.left
-        return current
         
     def _count_depth(self, node):
         """|INTERNAL| recursevly counts depth
@@ -88,7 +130,44 @@ class BinTree():
             depth_left = self._count_depth(node.left)
             depth_right = self._count_depth(node.right)
             depth = 1 + (max(depth_left, depth_right))
-        return depth      
+        return depth
+    
+    def _write_element(self, file, element):
+        """|INTERNAL| write `element` to opened `file`."""
+        data_type = type(element)
+        if (data_type in (int, float)):
+            file.write(pack(str(data_type)[8], element))    # if int or float - just write element
+        elif (data_type in (str, bytes)):
+            file.write(pack("i", len(element)))             # if str or bytes - write length and then element itself
+            if (data_type == str):
+                file.write(element.encode())
+            else:
+                file.write(element)
+        else:
+            file.write(pack("i", len(element)))             # if list or tuple - write it's length and recursevly write it's contents
+            for e in element:
+                file.write(str(type(e))[8].encode("ascii")) # write contents data type
+                self._write_element(file, e)
+                
+    def _read_element(self, file, datatype):
+        """|INTERNAL| read element with given `datatype` from opened `file`."""
+        if (datatype in ("i", "f")):
+            return unpack(datatype, file.read(4))[0]
+        elif (datatype in ("s", "b")):
+            length = unpack("i", file.read(4))[0]
+            if (datatype == "s"):
+                return unpack("%ds" % length, file.read(length))[0].decode()
+            else:
+                return file.read(length)
+        else:
+            length = unpack("i", file.read(4))[0]
+            data = []
+            for i in range(length):
+                elem_type = unpack("s", file.read(1))[0].decode("ascii")
+                data.append( self._read_element(file, elem_type) )
+            if (datatype == "t"):
+                data = tuple(data)
+            return data
         
     @property
     def root(self):
@@ -113,7 +192,7 @@ class BinTree():
                 if (type(v) != datatype):
                     raise ValueError("Elements in `values` are have different types!")
             tree.insert(v)
-        return tree    
+        return tree
     
     def is_empty(self) -> bool: 
         """Whether this tree empty or not."""
@@ -191,3 +270,46 @@ class BinTree():
     def depth(self) -> int:
         """Get depth of this tree."""
         return self._count_depth(self._root)
+    
+    def write(self, filepath, writefunc=None):
+        """Write tree data to binary file at `filepath`.
+        If `writefunc` given - writes data using this function, otherwise writes itself if tree data type is standart (int, float, str, bytes, list, tuple).
+        `writefunc` takes opened file and node data as args."""
+        with open(filepath, "wb") as file:
+            if (not writefunc):
+                data_type = None
+                for n in nlr(self.root):
+                    if (not data_type):
+                        data_type = type(n.data)
+                        if (data_type not in (int, float, str, list, tuple, bytes)):
+                            raise RuntimeError("To write non-standart data types pass `writefunc`.")
+                    elif (type(n.data) != data_type):
+                        raise RuntimeError("To write tree all nodes must have same data type.")
+                file.write(str(data_type)[8].encode("ascii"))
+            for n in nlr(self.root):
+                if (writefunc):
+                    writefunc(file, n.data)
+                else:
+                    self._write_element(file, n.data)
+                    
+    def read(self, filepath, readfunc=None):
+        """Read tree data from binary file at `filepath`.
+        If `readfunc` given - reads data using this function, otherwise reads itself if saved data type is standart (int, float, str, bytes, list, tuple).
+        `readfunc` takes opened file as arg."""
+        self.clear()
+        with open(filepath, "rb") as file:
+            if (not readfunc):
+                raw = file.read(1)
+                if (not raw.isalpha()):
+                    raise ValueError("File '%s': readfunc required or invalid tree data file." % filepath)
+                data_type = unpack("s", raw)[0].decode("ascii")
+            while (True):
+                pos = file.tell()
+                peek = file.read(1)
+                if (not peek):
+                    break
+                file.seek(pos)
+                if (readfunc):
+                    self.insert(readfunc(file))
+                else:
+                    self.insert(self._read_element(file, data_type))
